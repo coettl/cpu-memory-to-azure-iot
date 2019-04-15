c4b094dd5f79bb666ffc2879b4934ae1c716e20d
c.oettl <c.oettl@linemetrics.com>
2019-04-15 10:38:50 +0200

    .

--- cpu-memory-sender.js
+++ cpu-memory-sender.js
@@ -37,6 +37,34 @@ client.getTwin((err, receivedTwin) => {
     listenToSendTemperature();
 });
 
+function onBlink(request, response) {
+    // Function to send a direct method reponse to your IoT hub.
+    function directMethodResponse(err) {
+        if (err) {
+            console.error(
+                chalk.red(
+                    "An error ocurred when sending a method response:\n" +
+                        err.toString()
+                )
+            );
+        } else {
+            console.log(
+                chalk.green(
+                    "Response to method '" +
+                        request.methodName +
+                        "' sent successfully."
+                )
+            );
+        }
+    }
+
+    console.log(chalk.green("Direct method payload received:"));
+    console.log(chalk.green(request.payload));
+    blinkLed5Seconds();
+    response.send(200, "Successfully blinked!", directMethodResponse);
+}
+client.onDeviceMethod("blink", onBlink);
+
 let sendIntervalId = null;
 function startSendingData() {
     const sendInterval = reportedPropertiesPatch.sendInterval;
